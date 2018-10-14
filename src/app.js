a84a9e77d9478b356f66989f3e6691a8de294926
nichatr <nichatr@gmail.com>
2018-10-14 22:57:12 +0300

    added property files.filename in app.js, added app.md with remarks

--- src/app.js
+++ src/app.js
@@ -3,28 +3,28 @@ const klawSync = require('klaw-sync');
 const path = require('path');
 const _ = require('lodash');
 
-
-// const files = klawSync('./', { nodir: true });
-// var workPath = path.parse(files[5].path).dir;
-// var workFile = path.parse(files[5].path).base;
-// console.log(workPath);
-// console.log(__dirname);
-// var newPath = workPath.replace(__dirname, "");
-// console.log(`.${newPath}\\${workFile}`);
-
+// get in object all files, exclude directories.
 const files = klawSync('./', { nodir: true });
+
 _.forEach(files, function (value, key) {
-  var workPath = path.parse(value.path).dir;
-  var workFile = path.parse(value.path).base;
-  var newPath = workPath.replace(__dirname, "");
-  if (newPath) {
-    console.log(`.${newPath}\\${workFile}`);
-  } else {
-    console.log(workFile);
-  }
+    var workPath = path.parse(value.path).dir;
+    var workFile = path.parse(value.path).base;
+
+    //  remove the path up to the current working directory.
+    var newPath = workPath.replace(__dirname, "");
+
+    // add a new property to the object <files> with the file name.
+    // if file in sub-directory add "./sub-directory" to filename.
+    if (newPath) {
+        value.filename = `${newPath}\\${workFile}`;
+    } else {
+        value.filename = workFile;
+    }
 });
 
-
+_.forEach(files, function (value, key) {
+    console.log(value.filename);
+});
 
 
 
